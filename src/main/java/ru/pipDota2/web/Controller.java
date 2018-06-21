a9af2c912c2e990c4d23247e8bf3f2d9b5e3c8a4
chMatvey <morganrush@yandex.ru>
2018-06-21 20:03:38 +0300

    spring boot start

---
+++ src/main/java/ru/pipDota2/web/Controller.java
@@ -0,0 +1,13 @@
+package ru.pipDota2.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String getHello(){
+        return "Hello World";
+    }
+}
